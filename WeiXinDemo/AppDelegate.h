69b759e800559c186e2df1f3b06de798d72c33d7
tengsong <shawnt22@gmail.com>
2012-11-14 17:06:48 +0800

    Initial Commit

---
+++ WeiXinDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeiXinDemo
+//
+//  Created by 滕 松 on 12-11-14.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
